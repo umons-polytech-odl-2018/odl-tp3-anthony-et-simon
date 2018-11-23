c9dc8915aae9bc11779bf76df592a4061654af95
Anthony <161843@umons.ac.be>
2018-11-23 10:23:27 +0100

    form

---
+++ src/main/java/Formulaire.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Formulaire {
+	private JPanel mainPanel;
+	private JPanel drawingPanel;
+	private JPanel controls;
+	private JRadioButton Rectangle;
+	private JRadioButton squareRadioButton;
+	private JRadioButton ellipsisRadioButton;
+	private JRadioButton circleRadioButton;
+	private JTextPane messageZone;
+
+	private void createUIComponents() {
+		// TODO: place custom component creation code here
+	}
+}
--- src/main/java/be/ac/umons/odl/DrawingPanel.java
+++ src/main/java/be/ac/umons/odl/DrawingPanel.java
@@ -2,20 +2,23 @@ package be.ac.umons.odl;
 
 import java.awt.*;
 
-public class DrawingPanel extends javax.swing.JPanel {
+public class DrawingPanel extends javax.swing.JPanel
+{
 	private Drawable[] drawables = new Drawable[]{
 		new Square(new Point(100, 100), 100)
 	};
 
 	@Override
-	protected void paintComponent(Graphics g) {
+	protected void paintComponent(Graphics g)
+	{
 		super.paintComponent(g);
 		for (Drawable drawable : drawables) {
 			drawable.draw(g);
 		}
 	}
 
-	public void addDrawable(Drawable drawable) {
+	public void addDrawable(Drawable drawable)
+	{
 		Drawable[] drawables = new Drawable[this.drawables.length + 1];
 		System.arraycopy(this.drawables, 0, drawables, 0, this.drawables.length);
 		drawables[this.drawables.length] = drawable;
